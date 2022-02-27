0290efd34c57996abff83ebdb36539f04a42bfb0
Adriano <ampontes93@gmail.com>
2022-02-27 20:38:41 -0300

    failed attempt to add icon, still missing the icon and way to remove when uninstalling

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,6 +15,10 @@ Install-ChocolateyZipPackage @packageArgs
 try {
   cp $toolsPath\jmeter.cmd $env:ChocolateyInstall\bin
   cp $toolsPath\jmeterw.cmd $env:ChocolateyInstall\bin
+#   Install-ChocolateyShortcut `
+#     -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JMeter.lnk" `
+#     -TargetPath "C:\ProgramData\chocolatey\lib\jmeter\tools\apache-jmeter-5.4.3\bin\ApacheJMeter.jar" `
+#     -IconLocation "C:\test.ico" `
 } catch {
   throw $_.Exception.Message
 }
