454ba022c7cf618ca26c3b9a0e66266e361b234e
Adriano <ampontes93@gmail.com>
2022-02-27 20:08:32 -0300

    updating to jmeter 5.4.3

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'jmeter'
+  url            = 'https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.4.3.zip'
+  checksum       = '1ebc2a179d724aa58ff8b8f1c2146204208aeeeb8ba2b53168d6700be2d516a204b7e65dd94a6a0e3b84906fd33a97fcf2f2e6e44fb9b8fafa017c0c1856e1d8'
+  checksumType   = 'sha512'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+# Custom batch shim
+try {
+  cp $toolsPath\jmeter.cmd $env:ChocolateyInstall\bin
+  cp $toolsPath\jmeterw.cmd $env:ChocolateyInstall\bin
+} catch {
+  throw $_.Exception.Message
+}
+
+# environments registration
+$zipName = [System.IO.Path]::GetFileNameWithoutExtension($packageArgs.url)
+$env:JMETER_HOME = Join-Path $toolsPath $zipName
+try {
+  [Environment]::SetEnvironmentVariable('JMETER_HOME', $env:JMETER_HOME, 'Machine')
+} catch {
+  throw $_.Exception.Message
+}
---
+++ tools/jmeterw.cmd
@@ -0,0 +1,1 @@
+@call %ChocolateyInstall%\lib\jmeter\tools\apache-jmeter-5.4.3\bin\jmeterw.cmd %*
